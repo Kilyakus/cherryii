9b535f6707367413c826a41338b4863fe569e6c6
Kilyakus <aleksandr.kilyakov@gmail.com>
2026-04-24 13:41:04 +0500

    main

--- views/layouts/frontend-toolbar.php
+++ views/layouts/frontend-toolbar.php
@@ -9,16 +9,29 @@ $position = Setting::get('toolbar_position') === 'bottom' ? 'bottom' : 'top';
 $this->registerCss('body {padding-'.$position.': 50px;}');
 ?>
 <link href='http://fonts.googleapis.com/css?family=Open+Sans:400,300,600,700&subset=latin,cyrillic' rel='stylesheet' type='text/css'>
-<nav id="cherryii-navbar" class="navbar navbar-inverse navbar-fixed-<?= $position ?>">
+<nav id="cherryii-navbar" class="navbar navbar-dark bg-dark fixed-<?= $position ?> navbar-expand">
     <div class="container">
-        <ul class="nav navbar-nav navbar-left">
-            <li><a href="<?= Url::to(['/admin']) ?>"><span class="glyphicon glyphicon-arrow-left"></span> <?= Yii::t('cherryii', 'Control Panel') ?></a></li>
+        <ul class="navbar-nav me-auto">
+            <li class="nav-item">
+                <a class="nav-link" href="<?= Url::to(['/admin']) ?>">
+                    <i class="bi bi-arrow-left"></i> <?= Yii::t('cherryii', 'Control Panel') ?>
+                </a>
+            </li>
         </ul>
-        <p class="navbar-text"><i class="glyphicon glyphicon-pencil"></i> <?= Yii::t('cherryii', 'Live edit') ?></p>
-        <?= Html::checkbox('', LIVE_EDIT, ['data-link' => Url::to(['/admin/system/live-edit'])]) ?>
+        <span class="navbar-text d-flex align-items-center">
+            <i class="bi bi-pencil me-2"></i> <?= Yii::t('cherryii', 'Live edit') ?>
+            <?= Html::checkbox('', LIVE_EDIT, [
+                'data-link' => Url::to(['/admin/system/live-edit']),
+                'class' => 'form-check-input ms-2'
+            ]) ?>
+        </span>
 
-        <ul class="nav navbar-nav navbar-right">
-            <li><a href="<?= Url::to(['/admin/sign/out']) ?>"><span class="glyphicon glyphicon-log-out"></span> <?= Yii::t('cherryii', 'Logout') ?></a></li>
+        <ul class="navbar-nav ms-auto">
+            <li class="nav-item">
+                <a class="nav-link" href="<?= Url::to(['/admin/sign/out']) ?>">
+                    <i class="bi bi-box-arrow-right"></i> <?= Yii::t('cherryii', 'Logout') ?>
+                </a>
+            </li>
         </ul>
     </div>
 </nav>
--- views/layouts/main.php
+++ views/layouts/main.php
@@ -7,9 +7,11 @@ $asset = AdminAsset::register($this);
 $moduleName = $this->context->module->id;
 ?>
 <?php $this->beginPage() ?>
+<!DOCTYPE html>
 <html lang="<?= Yii::$app->language ?>">
 <head>
     <meta charset="<?= Yii::$app->charset ?>"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <?= Html::csrfMetaTags() ?>
     <title><?= Yii::t('cherryii', 'Control Panel') ?> - <?= Html::encode($this->title) ?></title>
     <link href='http://fonts.googleapis.com/css?family=Open+Sans:400,300,600,700&subset=latin,cyrillic' rel='stylesheet' type='text/css'>
@@ -21,59 +23,66 @@ $moduleName = $this->context->module->id;
 <?php $this->beginBody() ?>
 <div id="admin-body">
     <div class="wrapper">
-        <div class="header">
+        <div class="header clearfix">
             <div class="logo">
                 <img src="<?= $asset->baseUrl ?>/img/logo_20.png">
                 «CherrYii» CMS
             </div>
-            <div class="nav">
-                <a href="<?= Url::to(['/']) ?>" class="pull-left"><i class="glyphicon glyphicon-home"></i> <?= Yii::t('cherryii', 'Open site') ?></a>
-                <a href="<?= Url::to(['/admin/sign/out']) ?>" class="pull-right"><i class="glyphicon glyphicon-log-out"></i> <?= Yii::t('cherryii', 'Logout') ?></a>
+            <div class="nav px-3">
+                <a href="<?= Url::to(['/']) ?>" class="float-start text-decoration-none">
+                    <i class="bi bi-house"></i> <?= Yii::t('cherryii', 'Open site') ?>
+                </a>
+                <a href="<?= Url::to(['/admin/sign/out']) ?>" class="float-end text-decoration-none">
+                    <i class="bi bi-box-arrow-right"></i> <?= Yii::t('cherryii', 'Logout') ?>
+                </a>
             </div>
         </div>
-        <div class="main">
-            <div class="box sidebar">
+        <div class="main-container d-flex">
+            <div class="sidebar flex-shrink-0">
                 <?php foreach(Yii::$app->getModule('admin')->activeModules as $module) : ?>
                     <a href="<?= Url::to(["/admin/$module->name"]) ?>" class="menu-item <?= ($moduleName == $module->name ? 'active' : '') ?>">
                         <?php if($module->icon != '') : ?>
-                            <i class="glyphicon glyphicon-<?= $module->icon ?>"></i>
+                            <i class="bi bi-<?= $module->icon ?>"></i>
                         <?php endif; ?>
                         <?= $module->title ?>
                         <?php if($module->notice > 0) : ?>
-                            <span class="badge"><?= $module->notice ?></span>
+                            <span class="badge rounded-pill bg-light text-dark"><?= $module->notice ?></span>
                         <?php endif; ?>
                     </a>
                 <?php endforeach; ?>
                 <a href="<?= Url::to(['/admin/settings']) ?>" class="menu-item <?= ($moduleName == 'admin' && $this->context->id == 'settings') ? 'active' :'' ?>">
-                    <i class="glyphicon glyphicon-cog"></i>
+                    <i class="bi bi-gear"></i>
                     <?= Yii::t('cherryii', 'Settings') ?>
                 </a>
                 <?php if(IS_ROOT) : ?>
                     <a href="<?= Url::to(['/admin/modules']) ?>" class="menu-item <?= ($moduleName == 'admin' && $this->context->id == 'modules') ? 'active' :'' ?>">
-                        <i class="glyphicon glyphicon-folder-close"></i>
+                        <i class="bi bi-folder"></i>
                         <?= Yii::t('cherryii', 'Modules') ?>
                     </a>
                     <a href="<?= Url::to(['/admin/admins']) ?>" class="menu-item <?= ($moduleName == 'admin' && $this->context->id == 'admins') ? 'active' :'' ?>">
-                        <i class="glyphicon glyphicon-user"></i>
+                        <i class="bi bi-people"></i>
                         <?= Yii::t('cherryii', 'Admins') ?>
                     </a>
                     <a href="<?= Url::to(['/admin/system']) ?>" class="menu-item <?= ($moduleName == 'admin' && $this->context->id == 'system') ? 'active' :'' ?>">
-                        <i class="glyphicon glyphicon-hdd"></i>
+                        <i class="bi bi-hdd-network"></i>
                         <?= Yii::t('cherryii', 'System') ?>
                     </a>
                     <a href="<?= Url::to(['/admin/logs']) ?>" class="menu-item <?= ($moduleName == 'admin' && $this->context->id == 'logs') ? 'active' :'' ?>">
-                        <i class="glyphicon glyphicon-align-justify"></i>
+                        <i class="bi bi-list-check"></i>
                         <?= Yii::t('cherryii', 'Logs') ?>
                     </a>
                 <?php endif; ?>
             </div>
-            <div class="box content">
+            <div class="content w-100">
                 <div class="page-title">
                     <?= $this->title ?>
                 </div>
-                <div class="container-fluid">
+                <div class="container-fluid py-4">
                     <?php foreach(Yii::$app->session->getAllFlashes() as $key => $message) : ?>
-                        <div class="alert alert-<?= $key ?>"><?= $message ?></div>
+                        <div class="alert alert-<?= $key ?> alert-dismissible fade show" role="alert">
+                            <?= $message ?>
+                            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                        </div>
                     <?php endforeach; ?>
                     <?= $content ?>
                 </div>
